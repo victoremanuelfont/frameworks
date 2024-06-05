063c16afc5b4cb7957b8d2c9065805b1d6b910aa
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-06-05 14:11:46 -0300

    Indicando quais componentes dependem de quais

--- src/main/java/com/devsuperior/aula/AulaApplication.java
+++ src/main/java/com/devsuperior/aula/AulaApplication.java
@@ -1,11 +1,21 @@
 package com.devsuperior.aula;
 
+import com.devsuperior.entities.Employee;
+import com.devsuperior.services.SalaryService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.devsuperior"}) // Para corrigir um erro na hora de criação dos pacotes
 public class AulaApplication implements CommandLineRunner {
+
+	private SalaryService salaryService;
+
+	public AulaApplication(SalaryService salaryService) {
+		this.salaryService = salaryService;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaApplication.class, args);
@@ -13,6 +23,7 @@ public class AulaApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Olá mundo");
+		Employee employee = new Employee("Maria", 4000.00);
+		System.out.println(salaryService.netSalary(employee));
 	}
 }
