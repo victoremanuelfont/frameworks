0a35bbc367392587cc9e2da2d86f52816538098e
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-06-05 13:54:45 -0300

    Implementação das classes Employee e classes do pacote Services

---
+++ src/main/java/com/devsuperior/services/PensionService.java
@@ -0,0 +1,8 @@
+package com.devsuperior.services;
+
+public class PensionService {
+    public double discount(double amount){
+        return amount * 0.1;
+    }
+}
+
---
+++ src/main/java/com/devsuperior/services/SalaryService.java
@@ -0,0 +1,23 @@
+package com.devsuperior.services;
+
+import entities.Employee;
+
+public class SalaryService  {
+
+
+    private TaxService taxService;
+    private PensionService pensionService;
+
+
+    public SalaryService( TaxService taxService, PensionService pensionService) {
+        this.pensionService = pensionService;
+        this.taxService = taxService;
+    }
+
+    public double netSalary(Employee employee){
+        return employee.getGrossSalary() - taxService.tax(employee.getGrossSalary()) - pensionService.discount(employee.getGrossSalary());
+    }
+
+
+
+}
