63bc1bed0acad199b392e25a40db447b271f988d
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-06-05 14:15:25 -0300

    Forma alternativa de indicar quais componentes dependem de quais: @Autowired

--- src/main/java/com/devsuperior/aula/AulaApplication.java
+++ src/main/java/com/devsuperior/aula/AulaApplication.java
@@ -2,6 +2,7 @@ package com.devsuperior.aula;
 
 import com.devsuperior.entities.Employee;
 import com.devsuperior.services.SalaryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,11 +12,9 @@ import org.springframework.context.annotation.ComponentScan;
 @ComponentScan({"com.devsuperior"}) // Para corrigir um erro na hora de criação dos pacotes
 public class AulaApplication implements CommandLineRunner {
 
+	@Autowired
 	private SalaryService salaryService;
 
-	public AulaApplication(SalaryService salaryService) {
-		this.salaryService = salaryService;
-	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AulaApplication.class, args);
--- src/main/java/com/devsuperior/services/SalaryService.java
+++ src/main/java/com/devsuperior/services/SalaryService.java
@@ -2,20 +2,18 @@ package com.devsuperior.services;
 
 
 import com.devsuperior.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SalaryService  {
 
-
+    @Autowired
     private TaxService taxService;
+    @Autowired
     private PensionService pensionService;
 
 
-    public SalaryService( TaxService taxService, PensionService pensionService) {
-        this.pensionService = pensionService;
-        this.taxService = taxService;
-    }
 
     public double netSalary(Employee employee){
         return employee.getGrossSalary() - taxService.tax(employee.getGrossSalary()) - pensionService.discount(employee.getGrossSalary());
